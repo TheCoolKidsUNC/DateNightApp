75a9ca247e3975102206a79979d306a8973adf5b
Brea Torres <abreaw328@gmail.com>
2017-11-11 11:33:18 -0500

    added table row HTML functions for movie and resturant apis data

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -44,3 +44,83 @@ var dinnerQuery = function(cuisine, long, lat) {
 
 //test
 //dinnerQuery(userData.cuisinePref, userData.long, userData.lat);
+
+
+
+
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
+// 
+// Brea's working area
+// 
+// --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
+
+
+console.log(createTableRowMovie("choiceID", "Showtime At the Movies", ["7:30 PM", "8:15 PM", "9:45 PM", "10:35 PM"], "405 Watch This Way", "5.9"));
+console.log(createTableRowRestaruant("restaurantID", "Chow Down Here", "$$", "350 Dinner Way", "4.5"));
+
+// ---------------------------------------------------------------------------------------------------------------
+// Create & Format table row data for movie API data
+// arguments: movie name, times, location, miles away
+// returns: html setup of the new table row with the table data passed in the arugments section
+// ---------------------------------------------------------------------------------------------------------------
+function createTableRowMovie(id, name, timesArray, location, milesAway) {
+
+    var timesHTMLList = "<ul>";
+
+    for (var i = 0; i < timesArray.length; i++) {
+
+        timesHTMLList += `<li>${timesArray[i]}</li>`;
+    }
+
+    timesArray += "</ul>";
+
+    // use ` instead of ' or " to be able to add the variable names into the string and it interpret them for the values passed in
+    return `
+        <tr id='${id}'>
+            <td>${name}</td>
+            <td>${timesHTMLList}</td>
+            <td>${location}</td>
+            <td>${milesAway}</td>
+        </tr>
+    `;
+
+}
+
+// ---------------------------------------------------------------------------------------------------------------
+// Create & Format table row data for resturant API data
+// arguments: restauratn name, price range, location (address), miles away
+// returns: html setup of the new table row with the table data passed in the arugments section
+// ---------------------------------------------------------------------------------------------------------------
+function createTableRowRestaruant(id, name, price, location, milesAway) {
+
+    // use ` instead of ' or " to be able to add the variable names into the string and it interpret them for the values passed in
+    return `
+        <tr id='${id}'>
+            <td>${name}</td>
+            <td>${price}</td>
+            <td>${location}</td>
+            <td>${milesAway}</td>
+        </tr>
+    `;
+
+}
+
+
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
+// 
+// End of Brea's working area
+//
+// --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
